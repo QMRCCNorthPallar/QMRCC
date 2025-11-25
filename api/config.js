e3f0f422007b93092e400894cb32a2817a5f07e1
QMRCCNorthPallar <qmrccnorthpallar@gmail.com>
2025-11-25 19:29:09 +0530

    pass

--- api/config.js
+++ api/config.js
@@ -1,3 +1,3 @@
 export const ADMIN_PASSWORD = "QMRCC@@"; // Same as admin.js
-export const BLOB_READ_WRITE_TOKEN = "yourBlobToken";
-export const BLOB_PUBLIC_URL_BASE = "https://yourproject.vercel.app/blob";
+export const BLOB_READ_WRITE_TOKEN = "vercel_blob_rw_7kYvWztbQ4fUqzql_budRbMmsfvLIXiUa95BS58CVvIbGq0";
+export const BLOB_PUBLIC_URL_BASE = "https://7kyvwztbq4fuqzql.public.blob.vercel-storage.com";
