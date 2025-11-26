170001908886ba4bdfeef1da829cce080a2ea95c
QMRCCNorthPallar <qmrccnorthpallar@gmail.com>
2025-11-26 06:38:56 +0530

    pullback

--- config.js
+++
@@ -1,3 +0,0 @@
-export const ADMIN_PASSWORD = "QMRCC@@"; // Same as admin.js
-export const BLOB_READ_WRITE_TOKEN = "vercel_blob_rw_7kYvWztbQ4fUqzql_budRbMmsfvLIXiUa95BS58CVvIbGq0";
-export const BLOB_PUBLIC_URL_BASE = "https://7kyvwztbq4fuqzql.public.blob.vercel-storage.com";
--- worker/worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    return new Response("Cloudflare Worker running!", {
-      status: 200
-    });
-  }
-}
